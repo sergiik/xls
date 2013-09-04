7229d4b3272ce71f9db5f190b1c01934d34752ec
kiser <kipot.sergey@gmail.com>
2013-09-04 12:32:57 +0000

    print sheets info

--- read_cps.pl
+++ read_cps.pl
@@ -8,5 +8,8 @@ use English qw( -no_match_vars );
 
 my $book = ReadData("cps.xls");
 
-my $d = Data::Dumper->new($book);
-print $d->Dump or croak $OS_ERROR;
+#my $d = Data::Dumper->new($book->[0]);
+#print $d->Dump or croak $OS_ERROR;
+
+while (my ($k,$v)=each $book->[0] ){say "$k $v"}
+while (my ($k,$v)=each $book->[0]{sheet}){say "$k $v"}
