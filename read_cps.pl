526940dd6b1d995b060bf597c577c837da57bd80
U-NANTERRE\sergiKIP <sergiKIP@FRPAUAKI-04027.nanterre.oberthurcs.com>
2013-09-04 14:46:07 +0200

    cli arguments

--- read_cps.pl
+++ read_cps.pl
@@ -4,9 +4,26 @@ use autodie;
 
 use Data::Dumper;
 use Spreadsheet::Read;
+use Getopt::Long;
 use English qw( -no_match_vars );
 
-my $book = ReadData("cps.xls");
+my $xlsfile;
+my $help;
+
+GetOptions(
+    'help|?'    => \$help,
+    'xlsfile=s' => \$xlsfile,
+) or pod2usage(2);
+
+if ( defined $help ) {
+    pod2usage( -exitstatus => 0, -verbose => 2 );
+}
+
+if ( not defined $xlsfile ) {
+    pod2usage(2);
+}
+
+my $book = ReadData($xlsfile);
 
 #my $d = Data::Dumper->new($book->[0]);
 #print $d->Dump or croak $OS_ERROR;
