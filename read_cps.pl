9d07a680848bdbc2adf6e5107e4207773dcfaec4
U-NANTERRE\sergiKIP <sergiKIP@FRPAUAKI-04027.nanterre.oberthurcs.com>
2013-09-04 14:36:31 +0200

    perltidy

--- read_cps.pl
+++ read_cps.pl
@@ -11,5 +11,5 @@ my $book = ReadData("cps.xls");
 #my $d = Data::Dumper->new($book->[0]);
 #print $d->Dump or croak $OS_ERROR;
 
-while (my ($k,$v)=each $book->[0] ){say "$k $v"}
-while (my ($k,$v)=each $book->[0]{sheet}){say "$k $v"}
+while ( my ( $k, $v ) = each $book->[0] )        { say "$k $v" }
+while ( my ( $k, $v ) = each $book->[0]{sheet} ) { say "$k $v" }
