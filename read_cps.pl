0bb1889f9912484b6c510e8fcc32ea5bf869708b
U-NANTERRE\sergiKIP <sergiKIP@FRPAUAKI-04027.nanterre.oberthurcs.com>
2013-09-04 14:17:36 +0200

    second commit

--- read_cps.pl
+++ read_cps.pl
@@ -1,3 +1,12 @@
 #!/usr/bin/env perl
 use Modern::Perl 2011;
 use autodie;
+
+use Data::Dumper;
+use Spreadsheet::Read;
+use English qw( -no_match_vars );
+
+my $book = ReadData("cps.xls");
+
+my $d = Data::Dumper->new($book);
+print $d->Dump or croak $OS_ERROR;
