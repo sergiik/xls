6a6def8203fa1b18c2c136409f35f9831ebfb2b7
U-NANTERRE\sergiKIP <sergiKIP@FRPAUAKI-04027.nanterre.oberthurcs.com>
2013-09-04 17:42:38 +0200

    some fices

--- read_cps.pl
+++ read_cps.pl
@@ -9,12 +9,12 @@ use Getopt::Long;
 use English qw( -no_match_vars );
 use Readonly;
 
-Readonly my $num => 1;
-Readonly my $header_idx => 0;
+Readonly my $NUM => 1;
+Readonly my $HEADER_IDX => 0;
 
 my $xlsfile;
 my $help;
-my $sheet_num = $num;
+my $sheet_num = $NUM;
 
 GetOptions(
     'help|?'    => \$help,
@@ -41,19 +41,23 @@ my @rows = rows( $book->[$sheet_num] );
 
 my $i = 0;
 while ( $i <= 5 ) {
-    say join ";", @{ $rows[$i] };
+    say join q{;}, @{ $rows[$i] };
     $i++;
 }
 
-my $r = Record->new( header_names => $rows[$header_idx]);
+my $r = Record->new( header_names => $rows[$HEADER_IDX] );
 
 package Record;
 use Mouse;
 
-has 'header_names' => (is => 'ro', isa => 'ArrayRef', trigger => sub { 
-		my $self = shift;
-		say join ";", @{$self->header_names} 
-	}
+has 'header_names' => (
+    is      => 'ro',
+    isa     => 'ArrayRef',
+    trigger => sub {
+        my $self = shift;
+        say join q{;}, @{ $self->header_names };
+    },
 );
 
 __PACKAGE__->meta->make_immutable();
+
